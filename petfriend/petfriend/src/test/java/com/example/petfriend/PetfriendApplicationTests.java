295f0da01c349eafaedd1d6f232471bc325f89ff
sinhyeok-Oh <sinhyeok1390@naver.com>
2025-09-04 15:17:36 +0900

    spring io settings

---
+++ petfriend/petfriend/src/test/java/com/example/petfriend/PetfriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetfriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

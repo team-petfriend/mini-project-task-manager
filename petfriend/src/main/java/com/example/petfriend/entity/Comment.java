0c51c0452d86c9abee162facf3d13d496b1f5e4f
AKBINJ <jyp10311@naver.com>
2025-09-08 17:49:00 +0900

    comment

--- petfriend/src/main/java/com/example/petfriend/entity/Comment.java
+++ petfriend/src/main/java/com/example/petfriend/entity/Comment.java
@@ -1,12 +1,47 @@
 package com.example.petfriend.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Comment;
 
 @Entity
 @Table(name = "comment")
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = "task")
 public class Comment {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "task_id", nullable = false)
+    private Task task;
+
+    @Comment("댓글 내용")
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @Comment("댓글 작성자 표시명 또는 ID")
+    @Column(nullable = false, length = 100)
+    private String commenter;
+
+    private Comment(String content, String commenter) {
+        this.content = content;
+        this.commenter = commenter;
+    }
+
+    public static Comment create(String content, String commenter) {
+        return new Comment(content, commenter);
+    }
+
+    void setPost(Task post) {
+        this.task = post;
+    }
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
 }

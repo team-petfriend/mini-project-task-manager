8d14b9559f39524127922b35c6251aa370220805
AKBINJ <jyp10311@naver.com>
2025-09-11 17:42:32 +0900

    commentservice

--- petfriend/src/main/java/com/example/petfriend/service/impl/CommentServiceImpl.java
+++ petfriend/src/main/java/com/example/petfriend/service/impl/CommentServiceImpl.java
@@ -1,7 +1,13 @@
 package com.example.petfriend.service.impl;
 
+import com.example.petfriend.dto.ResponseDto;
+import com.example.petfriend.dto.comment.request.CommentCreateRequestDto;
+import com.example.petfriend.dto.comment.response.CommentResponseDto;
+import com.example.petfriend.entity.Task;
 import com.example.petfriend.repository.CommentRepository;
+import com.example.petfriend.repository.TaskRepository;
 import com.example.petfriend.service.CommentService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +17,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
+    private final TaskRepository taskRepository;
+
+    @Override
+    public ResponseDto<CommentResponseDto> createComment(Long taskId, CommentCreateRequestDto dto) {
+        
+
+        return null;
+    }
+
+    @Override
+    public ResponseDto<CommentResponseDto> deleteComment(Long taskId, Long commentId) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto<CommentResponseDto> updateComment(Long taskId, Long commentId, CommentCreateRequestDto dto) {
+        return null;
+    }
 }

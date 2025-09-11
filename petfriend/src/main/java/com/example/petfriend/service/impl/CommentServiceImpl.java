211bdc603118e3fd76dc07f2042ba85e30536f12
AKBINJ <jyp10311@naver.com>
2025-09-11 21:30:25 +0900

    fixcomment

--- petfriend/src/main/java/com/example/petfriend/service/impl/CommentServiceImpl.java
+++ petfriend/src/main/java/com/example/petfriend/service/impl/CommentServiceImpl.java
@@ -26,49 +26,52 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public ResponseDto<CommentResponseDto> createComment(Long taskId, CommentCreateRequestDto dto) {
-        Task task = taskRepository.findById(taskId)
-                .orElseThrow(() -> new EntityNotFoundException("해당 id의 Task를 찾을 수 없습니다."));
+//        Task task = taskRepository.findById(taskId)
+//                .orElseThrow(() -> new EntityNotFoundException("해당 id의 Task를 찾을 수 없습니다."));
+//
+//        User commenter = userRepository.findById(Long.valueOf(dto.commenter()))
+//                .orElseThrow(() -> new EntityNotFoundException("해당 Task의 사용자를 찾을 수 없습니다."));
+//
+//        Comments comment = Comments.create(dto.content(), commenter);
+//
+//        task.addComment(comment);
+//
+//        commentRepository.save(comment);
 
-        User commenter = userRepository.findById(Long.valueOf(dto.commenter()))
-                .orElseThrow(() -> new EntityNotFoundException("해당 Task의 사용자를 찾을 수 없습니다."));
-
-        Comments comment = Comments.create(dto.content(), commenter);
-
-        task.addComment(comment);
-
-        commentRepository.save(comment);
-
-        return ResponseDto.setSuccess("SUCCESS", CommentResponseDto.from(comment));
+//      return ResponseDto.setSuccess("SUCCESS", CommentResponseDto.from(comment));
+        return null;
     }
 
     @Override
     @Transactional
     public ResponseDto<CommentResponseDto> updateComment(Long taskId, Long commentId, CommentUpdateRequestDto dto) {
-        Comments comments = taskRepository.findById(commentId)
-                .orElseThrow(() -> new EntityNotFoundException("해당 id의 댓글을 찾을 수 없습니다."));
-
-        if(!comments.getTask().getId().equals(taskId)) {
-            throw new IllegalAccessException("해당 댓글이 Task 내에 속해있지 않습니다.");
-        }
-
-        comments.changeContent(dto.content());
-
-        return ResponseDto.setSuccess("SUCCESS", CommentResponseDto.from(comments));
+//        Comments comments = taskRepository.findById(commentId)
+//                .orElseThrow(() -> new EntityNotFoundException("해당 id의 댓글을 찾을 수 없습니다."));
+//
+//        if(!comments.getTask().getId().equals(taskId)) {
+//            throw new IllegalAccessException("해당 댓글이 Task 내에 속해있지 않습니다.");
+//        }
+//
+//        comments.changeContent(dto.content());
+//
+//        return ResponseDto.setSuccess("SUCCESS", CommentResponseDto.from(comments));
+        return null;
     }
 
     @Override
     @Transactional
     public ResponseDto<CommentResponseDto> deleteComment(Long taskId, Long commentId) {
-        Comments comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new EntityNotFoundException("해당 Task의 댓글을 찾을 수 없습니다."));
-
-        if (!comment.getTask().getId().equals(taskId)) {
-            throw new IllegalAccessException("해당 댓글이 Task 내에 속해있지 않습니다.")
-        }
-
-        Task task = comment.getTask();
-        task.removeComment(comment); // Task 엔티티에 removeComment 메서드 필요
-
-        return ResponseDto.setSuccess("SUCCESS", null);
+//        Comments comment = commentRepository.findById(commentId)
+//                .orElseThrow(() -> new EntityNotFoundException("해당 Task의 댓글을 찾을 수 없습니다."));
+//
+//        if (!comment.getTask().getId().equals(taskId)) {
+//            throw new IllegalAccessException("해당 댓글이 Task 내에 속해있지 않습니다.")
+//        }
+//
+//        Task task = comment.getTask();
+//        task.removeComment(comment); // Task 엔티티에 removeComment 메서드 필요
+//
+//        return ResponseDto.setSuccess("SUCCESS", null);
+        return null;
     }
 }

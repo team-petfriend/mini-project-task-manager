ce45d54a08a3158b78fd4f6c594b95e13468516d
AKBINJ <jyp10311@naver.com>
2025-09-11 17:00:25 +0900

    fix/comment

--- quary.sql
+++ quary.sql
@@ -138,7 +138,6 @@ USE `petfriend`;
 CREATE TABLE IF NOT EXISTS `comments` (
 	id			BIGINT NOT NULL AUTO_INCREMENT,
     task_id		BIGINT NOT NULL COMMENT 'tasks.id FK',
-    user_id 	BIGINT NOT NULL,
     content 	VARCHAR(500) NOT NULL COMMENT '댓글 내용',
     commenter 	VARCHAR(100) NOT NULL COMMENT '댓글 작성자 표시명 또는 ID',
     created_at	DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
@@ -146,8 +145,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
     PRIMARY KEY (`id`),
     KEY `idx_comment_task_id` (task_id),
     KEY `idx_comment_commenter` (commenter),
-    CONSTRAINT `fk_comments_task` 	FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT `fk_comments_user_id` FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT `fk_comments_task` 	FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci

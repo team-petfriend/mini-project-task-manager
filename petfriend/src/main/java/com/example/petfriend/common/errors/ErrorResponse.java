11eccd7bf023af0fe70ce15427e3bd5d88aff47e
jihoonjeong56 <jeong283056@gmail.com>
2025-09-18 10:39:56 +0900

    fix common & remove annotation

--- petfriend/src/main/java/com/example/petfriend/common/errors/ErrorResponse.java
+++ petfriend/src/main/java/com/example/petfriend/common/errors/ErrorResponse.java
@@ -3,9 +3,9 @@ package com.example.petfriend.common.errors;
 import java.util.List;
 
 public record ErrorResponse(
-        String code, // 에러 코드
-        String reason, // 사용자용 요약 메시지
-        List<FieldErrorItem> errors // 검증 실패시 필드 오류 목록
+        String code,
+        String reason,
+        List<FieldErrorItem> errors
 ) {
     public static ErrorResponse of(String code, String reason) {
         return new ErrorResponse(code, reason, List.of());

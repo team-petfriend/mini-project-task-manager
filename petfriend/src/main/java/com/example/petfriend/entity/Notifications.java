286f3490eca1ee2627ea6fc46b209798497c721a
AKBINJ <jyp10311@naver.com>
2025-09-24 16:07:42 +0900

    fix error

--- petfriend/src/main/java/com/example/petfriend/entity/Notifications.java
+++ petfriend/src/main/java/com/example/petfriend/entity/Notifications.java
@@ -5,6 +5,7 @@ import com.example.petfriend.common.enums.Type;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Notifications {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
